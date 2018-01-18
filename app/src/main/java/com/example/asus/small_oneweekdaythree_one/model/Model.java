e4847e10400b6a0b5ad94ac39818344552d54fd7
YokeyMi <2082870793@qq.com>
2018-01-18 10:43:39 +0800

    第二次提交到本地

---
+++ app/src/main/java/com/example/asus/small_oneweekdaythree_one/model/Model.java
@@ -0,0 +1,14 @@
+package com.example.asus.small_oneweekdaythree_one.model;
+
+import com.example.asus.small_oneweekdaythree_one.untils.HttpUntils;
+
+/**
+ * Created by asus on 2018/1/17.
+ */
+
+public class Model implements ModelInfo {
+    @Override
+    public void getDataFromWhere(String[] strings, CallBackLisnter callBackLisnter) {
+        HttpUntils.getDateByRe(callBackLisnter);
+    }
+}
